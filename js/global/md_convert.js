e376d1096cc1492d6ec65d3070a25963f73d441f
WilCahuaya <48217068@continental.edu.pe>
2023-04-17 17:35:08 -0500

    add: prism.js

---
+++ js/global/md_convert.js
@@ -0,0 +1,4 @@
+
+fetch("/md/componentes/index.md").then((response)=>response.text()).then(text=>{
+    document.getElementById("content").innerHTML = marked.parse(text);
+})
--- js/md_convert.js
+++
@@ -1,6 +0,0 @@
-var app = document.querySelector('#content');
-var md = fetch("/instructions.md").then((response)=>response.text()).then(text=>{
-    var converter = new showdown.Converter();
-    var html = converter.makeHtml(text);
-    app.innerHTML = html;
-})
