e915dbf5935e4678e94265594024345d28c689c4
WilCahuaya <48217068@continental.edu.pe>
2024-10-15 09:16:12 -0500

    add: anita byrthday

---
+++ anitaBirthday.js
@@ -0,0 +1,5 @@
+// Puedes agregar interactividad si lo deseas
+document.querySelector("form").addEventListener("submit", function (event) {
+  event.preventDefault();
+  alert("Mensaje enviado, ¡gracias!");
+});
