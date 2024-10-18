f5474d9853ee7b210ef1a7d532f6ffcd23d6b206
WilCahuaya <48217068@continental.edu.pe>
2024-10-18 02:08:06 -0500

    add: modal img

--- anitaBirthday.js
+++ anitaBirthday.js
@@ -2,9 +2,16 @@ const menuToggle = document.querySelector(".menu-toggle");
 const menu = document.querySelector(".header__nav");
 const closeIcon = document.querySelector(".closeIcon");
 const hamburgerIcon = document.querySelector(".hamburgerIcon");
+const modal = document.getElementById("modalImgBirthayDialog");
 
 menuToggle.addEventListener("click", () => {
   menu.classList.toggle("block");
   closeIcon.classList.toggle("block");
   hamburgerIcon.classList.toggle("none");
+});
+
+modal.addEventListener("click", (e) => {
+  if (e.target === modal) {
+    modal.close();
+  }
 });
