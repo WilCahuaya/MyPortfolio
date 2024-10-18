7153996f345cc6763012b2d6bc751be216cfc720
WilCahuaya <48217068@continental.edu.pe>
2024-10-18 11:21:17 -0500

    add: show and close modal

---
+++ anitaMemories.js
@@ -0,0 +1,99 @@
+const modal1 = document.querySelector(".modalImgDialog1");
+const modal2 = document.querySelector(".modalImgDialog2");
+const modal3 = document.querySelector(".modalImgDialog3");
+const modal4 = document.querySelector(".modalImgDialog4");
+const modal5 = document.querySelector(".modalImgDialog5");
+const modal6 = document.querySelector(".modalImgDialog6");
+const modal7 = document.querySelector(".modalImgDialog7");
+const modal8 = document.querySelector(".modalImgDialog8");
+const modal9 = document.querySelector(".modalImgDialog9");
+const modal10 = document.querySelector(".modalImgDialog10");
+const modal11 = document.querySelector(".modalImgDialog11");
+const modal12 = document.querySelector(".modalImgDialog12");
+const modal13 = document.querySelector(".modalImgDialog13");
+const modal14 = document.querySelector(".modalImgDialog14");
+
+modal1.addEventListener("click", (e) => {
+  console.log(e.target);
+  if (e.target === modal1) {
+    modal1.close();
+  }
+});
+
+modal2.addEventListener("click", (e) => {
+  if (e.target === modal2) {
+    modal2.close();
+  }
+});
+
+modal3.addEventListener("click", (e) => {
+  if (e.target === modal3) {
+    modal3.close();
+  }
+});
+
+modal4.addEventListener("click", (e) => {
+  if (e.target === modal4) {
+    modal4.close();
+  }
+});
+
+modal5.addEventListener("click", (e) => {
+  if (e.target === modal5) {
+    modal5.close();
+  }
+});
+
+modal6.addEventListener("click", (e) => {
+  if (e.target === modal6) {
+    modal6.close();
+  }
+});
+
+modal7.addEventListener("click", (e) => {
+  if (e.target === modal7) {
+    modal7.close();
+  }
+});
+
+modal8.addEventListener("click", (e) => {
+  if (e.target === modal8) {
+    modal8.close();
+  }
+});
+
+modal9.addEventListener("click", (e) => {
+  if (e.target === modal9) {
+    modal9.close();
+  }
+});
+
+modal10.addEventListener("click", (e) => {
+  if (e.target === modal10) {
+    modal10.close();
+  }
+});
+
+modal11.addEventListener("click", (e) => {
+  if (e.target === modal11) {
+    modal11.close();
+  }
+});
+
+modal12.addEventListener("click", (e) => {
+  if (e.target === modal12) {
+    modal12.close();
+  }
+});
+
+modal13.addEventListener("click", (e) => {
+  if (e.target === modal13) {
+    modal13.close();
+  }
+});
+
+modal14.addEventListener("click", (e) => {
+  if (e.target === modal14) {
+    modal14.close();
+  }
+});
