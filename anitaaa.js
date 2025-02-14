fc3b0061d1dc0968b28ee9f8dd2e58d0c4d14be3
WilCahuaya <48217068@continental.edu.pe>
2025-02-14 01:16:38 -0500

    update: anitaaa

--- anitaaa.js
+++ anitaaa.js
@@ -1,23 +1,25 @@
 const messages = [
-  `Hoy quiero recordarte lo especial que eres para mi,
-  valoro mucho las cosas que haces por mi y doy gracias a Dios por tu amistad,
-  a veces pienso que estamos escribiendo una bonita historia juntos,
-  no olvides que aqui tienes a alguien que le importas, 
+  `Hoy quiero recordarte lo especial que eres para mi.
+  Valoro mucho las cosas que haces por mi. 
+  Doy gracias a Dios por tu amistad.
+  A veces pienso que estamos escribiendo una bonita historia juntos.
+  No olvides que aqui tienes a alguien que le importas.
   que te quiere y piensa que eres lo mejor que le paso,
-  somos un buen equipo en Escuela Dominical, me gusta verte feliz
-  ya sabes si me necesitas cuando quieras, donde quieras y como quieras, 
-  doy gracias por tu vidaaa`,
+  Somos un buen equipo en Escuela Dominical.
+  Me gusta verte feliz.
+  Ya sabes si me necesitas cuando quieras, donde quieras y como quieras.
+  Doy gracias por tu vidaaa`,
 
-  `Me gusta cuando frunces los ojos, 
-  me gusta que siempre luces espectacular, 
-  me gusta tu sonrisa única, 
-  me gusta que te emociones al ver gatitos o perritos, 
-  me gusta que ames a los niños de Escuela Dominical, 
-  me gusta que seas comprometida con el ministerio, 
-  me gusta tu sensibilidad, 
-  me gusta que seas social, 
-  me gusta que cuando nos encontramos siempre conversemos mucho, 
-  me gusta que compartamos tantas cosas en común. 
+  `Me gusta cuando frunces los ojos.
+  Me gusta que siempre luces espectacular.
+  Me gusta tu sonrisa única.
+  Me gusta que te emociones al ver gatitos o perritos.
+  Me gusta que ames a los niños de Escuela Dominical.
+  Me gusta que seas comprometida con el ministerio.
+  Me gusta tu sensibilidad.
+  Me gusta que seas social.
+  Me gusta que cuando nos encontramos siempre conversemos mucho.
+  Me gusta que compartamos tantas cosas en común. 
   Gracias por tu bonita amistad`,
 
   ,
@@ -54,7 +56,7 @@ function showMessage(text) {
       span.style.color = colors[Math.floor(Math.random() * colors.length)];
 
       // Si la palabra tiene una coma, agregamos un salto de línea
-      span.innerHTML = words[i].includes(",")
+      span.innerHTML = words[i].includes(".")
         ? words[i] + "<br>"
         : words[i] + " ";
 
@@ -68,7 +70,7 @@ function showMessage(text) {
 
 // Función de confeti (duración 10 segundos)
 function launchConfetti() {
-  let duration = 10 * 1000;
+  let duration = 5 * 1000;
   let end = Date.now() + duration;
 
   (function frame() {
